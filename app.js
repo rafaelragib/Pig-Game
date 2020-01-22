f69cfbc464461dc930a809c5bd4a74bb6054dea6
ragib.nur <ragib08@gmail.com>
2020-01-22 20:43:38 +0600

    Fixed current score and button

--- app.js
+++ app.js
@@ -9,3 +9,31 @@ GAME RULES:
 
 */
 
+var score=[0,0];
+var active=0;
+var roundScore=0;
+//document.querySelector('#current-'+active).textContent=dice;
+
+document.querySelector('.dice').style.display='none';
+document.getElementById('current-0').textContent='0';
+document.getElementById('current-1').textContent='0';
+
+document.querySelector('.btn-roll').addEventListener('click',function()
+	{
+		var dice=Math.floor(Math.random()*6)+1;
+		//console.log(dice);
+		document.querySelector('.dice').src='dice-'+dice+'.png';
+		document.querySelector('.dice').style.display='block';
+		roundScore+=dice;
+		document.getElementById('current-'+active).textContent=roundScore;
+		if(dice===1)
+		{
+			roundScore=0;
+			document.getElementById('current-'+active).textContent=roundScore;
+			active===0 ? active=1 : active=0;
+			document.querySelector('.player-0-panel').classList.toggle('active');
+			document.querySelector('.player-1-panel').classList.toggle('active');
+			document.querySelector('.dice').style.display='none';
+			
+		}
+	});
