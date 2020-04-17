bd32c25e37a86d59d2aee6232d6f9b474fe32d5a
ragib.nur <ragib08@gmail.com>
2020-04-17 20:01:17 +0600

    Completed the pig game

--- app.js
+++ app.js
@@ -8,33 +8,48 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var score,active,roundScore,gameover,prevdice;
+var score,active,roundScore,gameover,prevdice,winningscore,onedice;
 init();
 
-
+//rolling the dice here
 document.querySelector('.btn-roll').addEventListener('click',function()
 	{
 		if(!gameover){
 		//var dice=6;
-		var dice=Math.floor(Math.random()*6)+1;
+		var dice=Math.floor(Math.random() * 6)+1 ;
+		if(dice===1)
+		{
+			onedice=1;
+		}	
 		//console.log(dice);
 		document.querySelector('.dice').src='dice-'+dice+'.png';
 		document.querySelector('.dice').style.display='block';
 		roundScore+=dice;
+		var dice=Math.floor(Math.random() * 6)+1 ;
+		document.querySelector('.dicesecond').src='dice-'+dice+'.png';
+		document.querySelector('.dicesecond').style.display='block';
+		roundScore+=dice;
+
 		document.getElementById('current-'+active).textContent=roundScore;
 		
-		if(dice===1)
+		if(dice===1 && onedice===1)
 		{
-
+			onedice=0;
 			nextPlayer();
 			//document.querySelector('.dice').style.display='none';
 
 		}
+		//if player rolls 6 in a row.
 		else if(dice===6 && prevdice===0)
 			prevdice=6;
 		else if(dice===6 && prevdice===6)
-			nextPlayer();
-
+			{
+				score[active]=0;
+				document.getElementById('score-'+active).textContent=score[active];
+				roundScore=0;
+				prevdice=0;
+				nextPlayer();
+			}
 
 
 	}
@@ -42,11 +57,13 @@ document.querySelector('.btn-roll').addEventListener('click',function()
 	});
 document.querySelector('.btn-hold').addEventListener('click',function()
 {
+	winningscore=parseInt(document.querySelector('.winnerinput').value);
+	winningscore
 	if(!gameover){
 	score[active]+=roundScore;
 	document.getElementById('score-'+active).textContent=score[active];
 	
-	if(score[active]>=20){
+	if(score[active]>=winningscore){
 		document.getElementById('name-'+active).textContent='Winner!';
 		document.querySelector('.dice').style.display='None';
 		document.querySelector('.player-'+active+'-panel').classList.add('winner');
@@ -79,9 +96,13 @@ active=0;
 roundScore=0;
 gameover=false;
 prevdice=0;
+winningscore=0;
+onedice=0;
 //document.querySelector('#current-'+active).textContent=dice;
 
 document.querySelector('.dice').style.display='none';
+document.querySelector('.dicesecond').style.display='none';
+
 document.getElementById('current-0').textContent='0';
 document.getElementById('current-1').textContent='0';
 document.getElementById('score-0').textContent='0';
